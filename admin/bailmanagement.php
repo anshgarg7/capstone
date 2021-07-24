eab031397b918e644d47f48aece2228e09802156
Tarun Kumar Dixit <43788843+tarun-sharma03@users.noreply.github.com>
2021-07-24 23:18:35 +0530

    bail management begin (change log)

--- admin/bailmanagement.php
+++ admin/bailmanagement.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Parole Management Dashboard</h1>
+                    <h1 class="m-0">Bail Management Dashboard</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -21,8 +21,7 @@
         <div class="container-fluid">
             <!-- Small boxes (Stat box) -->
             <div class="row">
-                <a href="#" class="btn btn-block btn-success">Pending Parole Requests</a>
-                <a href="createNewParole.php" class="btn btn-block btn-success">Create New Parole Entry</a>
+                <a href="#" class="btn btn-block btn-success">Pending Bail Orders</a>
 
             </div>
         </div>
@@ -32,7 +31,7 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">All Inmates on parole</h3>
+                        <h3 class="card-title">All Inmates on bail</h3>
 
                         <div class="card-tools">
                             <ul class="pagination pagination-sm float-right">
@@ -51,30 +50,33 @@
                                 <tr>
                                     <th style="width: 10px">#</th>
                                     <th>Name</th>
-                                    <th>UID Number</th>
-                                    <th>Date of Parole</th>
-                                    <th>Duration (Days)</th>
+                                    <th>Date of Court Order</th>
+                                    <th>Date of Approval</th>
                                     <th>Action</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
                                 $jailId = 1;
-                                $inmates = getThis("SELECT `id`, `inmateID`, `dateOfParole`, `durationOfParole` FROM `parolemanagement` WHERE `jailId`='$jailId' AND `paroleRequestStatus`='1' ORDER BY `id` DESC");
-                                for ($i = 0; $i < sizeof($inmates); $i++) {
-                                    $inmate = $inmates[$i];
+                                $bailInmates = getThis("SELECT `id`, `inmateID`, `orderID`, `approvalDate` FROM `bailmanagement` WHERE `jailID`='$jailId'");
+                                for ($i = 0; $i < sizeof($bailInmates); $i++) {
+                                    $inmate = $bailInmates[$i];
                                 ?>
                                     <tr>
-                                        <td><?php echo $i + 1; ?></td>
-                                        <?php
-                                        $inmateID = $inmate['id'];
-                                        $inmateDetails = getThis("SELECT `firstName`, `lastName`, `idProofNumber` FROM `inmatedetails` WHERE `id`='$inmateID'");
-                                        $inmateDetails = $inmateDetails[0];
-                                        ?>
-                                        <td><?php echo e_d('d', $inmateDetails['firstName']) . " " . e_d('d', $inmateDetails['lastName']); ?></td>
-                                        <td><?php echo e_d('d', $inmateDetails['idProofNumber']); ?></td>
-                                        <td><?php echo  e_d('d', $inmate['dateOfParole']); ?></td>
-                                        <td><?php echo e_d('d', $inmate['durationOfParole']); ?></td>
+                                        <td><?php echo ($i + 1); ?></td>
+                                        <td>
+                                            <?php $inmateId = $inmate['inmateID'];
+                                            $inmateName = getThis("SELECT `firstName`, `lastName` FROM `inmatedetails` WHERE `id`='$inmateId'");
+                                            $inmateName = $inmateName[0];
+                                            echo e_d('d', $inmateName['firstName']) . " " . e_d('d', $inmateName['lastName']); ?>
+                                        </td>
+                                        <td>
+                                            <?php $orderId = $inmate['orderID'];
+                                            $orderDetails = getThis("");
+                                            $orderDetails = $orderDetails[0]['date'];
+                                            echo e_d('d', $orderDetails); ?>
+                                        </td>
+                                        <td><?php echo e_d('d', $inmate['approvalDate']); ?></td>
                                         <td><a href="#" class="btn btn-primary">View Details</a></td>
                                     </tr>
                                 <?php
