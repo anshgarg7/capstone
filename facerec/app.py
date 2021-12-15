d3621a066e05e3082ab91df8b77f55d715515042
Ansh Garg <anshgarg7@gmail.com>
2021-12-15 13:20:37 +0530

    face rec

---
+++ facerec/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def run_script():
+    file = open(r'embeddings.py', 'r').read()
+    return exec(file)
+
+if __name__ == "__main__":
+    app.run(debug=True)
